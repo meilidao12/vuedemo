46439aeca6b36d60c16a14ae39f5c5a410b79fff
meilidao12 <330629898@qq.com>
2018-12-12 14:39:20 +0800

    123

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,34 @@
 <!-- 所有的内容要被根节点包含起来 -->
     <div>
         <h2>
-            这是一个首页组件
+            这是一个首页组件 
         </h2>
+        <br>
+        <button v-on:click="run()">获取msg</button>
     </div>
 </template>
+
+
+<script>
+export default {
+    data(){
+        return{
+            msg:'我是一个首页组件'
+        }
+    },
+    methods:{
+        run(){
+            alert(this.msg);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+h2{
+    color: red
+}
+</style>
+
+
+
