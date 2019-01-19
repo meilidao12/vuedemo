7d9cdd5a34d2e02f766524a0d7b1e161ceb8cd2e
meilidao12 <330629898@qq.com>
2019-01-19 11:16:07 +0800

    132

--- src/components/Page1.vue
+++ src/components/Page1.vue
@@ -3,3 +3,20 @@
         页面一
     </div>
 </template>
+
+<script>
+export default {
+    methods:{
+        getHub:function(){
+            axios.get('/user?ID=12345')
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    },
+    amount
+}
+</script>
