08429c732aba6951a0fa4de095352468bd5d1f5a
meilidao12 <330629898@qq.com>
2019-01-24 23:17:00 +0800

    12

--- src/components/Page2.vue
+++ src/components/Page2.vue
@@ -1,23 +1,50 @@
 <template>
     <div>
-        <button @click="Load()">请求数据</button>
+        <el-tree
+            :data="data2"
+            show-checkbox
+            default-expand-all
+            node-key="id"
+            ref="tree"
+            highlight-current
+            :props="defaultProps">
+        </el-tree>
     </div>
 </template>
 
 <script>
-import VueSocketio from 'vue-socket.io';
-import socketio from 'socket.io-client';
-Vue.use(VueSocketio, socketio('http://127.0.0.1:3000/'), store);
+
 export default {
-    methods:{
-        send:function()
-        {
-            this.$socket.emit('test', '123');
-        }
-    },
-    sockets: {
-      connect() {
-        console.log('socket connected');
+    data() {
+      return {
+        data2:[
+            {
+                id: 1,
+                label: '一级 1',
+                children: [{
+                    id: 4,
+                    label: '二级 1-1',
+                    children: [{
+                        id: 9,
+                        label: '三级 1-1-1'
+                        }, {
+                        id: 10,
+                        label: '三级 1-1-2'
+                    }]
+                }]
+            },
+            {
+                id: 2,
+                label: '一级 2',
+                children: [{
+                    id: 5,
+                    label: '二级 2-1'
+                }, {
+                    id: 6,
+                    label: '二级 2-2'
+                }]
+            }
+        ]
       }
     }
 }
